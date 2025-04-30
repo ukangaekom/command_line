89340feaf1f42570a08d252c3083c01e22d63db2
ukangaekom <ekomabasiuk@gmail.com>
2025-04-30 01:20:16 +0100

    updates

---
+++ README.md
@@ -0,0 +1,10 @@
+# Rust Command Line
+
+
+## Tutorial
+https://youtu.be/lFhQpVbXPtY
+
+
+## Complements
+1. https://youtu.be/ztsOwk1zB3o
+2. https://youtu.be/nYSE5ZZDAkU
---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use clap::{Arg, App}
+
+fn main() {
+    
+    let matches = App::new("AI StaticSol")
+        .version("1.0")
+        .author("Zealynx Security")
+        .about("An AI Powered Auditing Static Analyzer")
+        .arg()
+
+
+
+
+}
