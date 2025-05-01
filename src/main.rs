ec2b87b23268af970a47a3696ae2d88017521765
ukangaekom <ekomabasiuk@gmail.com>
2025-05-01 06:40:57 +0100

    changes

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ fn main() {
     
     let matches = App::new("AI StaticSol")
         .version("1.0")
-        .author("Zealynx Security")
+        .author("Zealynx Sec")
         .about("An AI Powered Auditing Static Analyzer")
         .arg()
 
