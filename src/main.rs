256d7648bd468e3061f7975dea546d8a8d853993
ukangaekom <ekomabasiuk@gmail.com>
2025-05-02 01:21:41 +0100

    zealynx

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,32 @@
-use clap::{Arg, App}
+use clap::{Arg, App};
 
 fn main() {
     
     let matches = App::new("AI StaticSol")
         .version("1.0")
         .author("Zealynx Sec")
-        .about("An AI Powered Auditing Static Analyzer")
-        .arg()
+        .about("An AI Powered Auditing Static Analyzer Command Line Demo!")
+        .arg(Arg::new("framework")
+            .long("framework")
+            .value_name("project_type")
+            // .about("Which type of solana project are you auditing?")
+            .takes_value(true),
+        )
+        .get_matches();
+
+        // TODO: select frameworks (options: [native_rust, anchor,])
 
 
 
+        if let Some(i) = matches.value_of("framework"){
+
+            match i {
+                "anchor" => println!("Traversing anchor project"),
+                "native_rust" => println!("Traversing native rust projects"),
+
+                _ => println!("The option {} isn't supported by the commandline tool",i),
+            }
+            
+        }
 
 }
